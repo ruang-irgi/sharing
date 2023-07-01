b384265167c7cb12a8edfaa03a140a7213ed8f04
GUANSRE <42710188+GUANSRE@users.noreply.github.com>
2023-07-01 14:03:29 +0700

    Add files via upload

---
+++ beranda1.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+	<title>MathGirls | Scincare </title>
+	<meta charset="utf-8">
+	<title>MathGirls</title>
+	<link href="theme.css" type="text/css" rel="stylesheet">
+	<link rel="icon" href="https://raw.githubusercontent.com/GUANSRE/sharing/main/logo.jpg">
+</head>
+
+<body style="background-image: url('https://raw.githubusercontent.com/GUANSRE/sharing/main/GIRLS.jpg'); background-size: 100%;" class="trans">
+	<div style="background-color: rgba(0, 0, 0, 0.75); width: 100%; height:1000px; margin-top: -35px;">
+		<center>
+			<div style="padding-top: 50px; padding-bottom:50px">
+				<h1><img src="https://raw.githubusercontent.com/GUANSRE/sharing/main/logo.jpg" style="width: 900px; height: 300px;"></h1>
+				<h1><font face="Futura Md BT" color="white">Selamat Datang di <u>MathGirls</u></h1>
+				<p style="font-family: Brush Script Std; font-size: 18px;">Pesan SCINCARE yang diinginkan hanya dengan 1 klik</p>
+				<a href="signin.php"><button>Klik untuk mencari</button></a>
+			</div>
+		</center>
+	</div>
+	
+<section id="footer">
+	<div class="footer1">
+		<div class="container">
+			<div class="row">
+			<table style="width:100%"><tr>
+				<td><div class="col-4 kolom-footer" style="color: white">
+					<h3>MathGirls</h3>
+							<img src="https://raw.githubusercontent.com/GUANSRE/sharing/main/logo.jpg" width="150" height="100">
+							<p>Beli Scincare dengan mudah.</p>
+							<p>Hanya di Math_Girls</p>
+					
+				</div></td>
+				<td><div class="col-13" style="color: white; margin-left:65%">
+					<h3>Silahkan Tanya Kami di </h3>
+						<img src="https://raw.githubusercontent.com/GUANSRE/sharing/main/IMG-20230701-WA0022.jpg" width="50" height="50">
+						<ul>
+								<li>@atikamaul_</li>
+								<li>@_putyy_</li>
+								<li>@rabiatulhp</li>
+						</ul>
+					</div>
+					<div class="footer2" style="margin-left:57%">Create by atika-puti-putri &copy; 2022. All Right Reserved</div>
+				</td>
+			</tr></table>
+			</div>
+		</div>
+	</div>
+	
+</section>
+</body>
